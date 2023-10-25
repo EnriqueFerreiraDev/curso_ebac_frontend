92b960d51e7058e0d479727d1632c8a0a1fe806e
EnriqueFerreiraDev <139347123+EnriqueFerreiraDev@users.noreply.github.com>
2023-10-25 13:42:13 -0300

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+      less: {
+        development: {
+          options: {
+            paths: ["assets/css"]
+          },
+          files: {
+            "dist/style.css": "src/style.less"
+          }
+        }
+      },
+      uglify: {
+        my_target: {
+          files: {
+            'dist/script.min.js': ['src/script.js']
+          }
+        }
+      }
+    });
+  
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+  
+    grunt.registerTask('default', ['less', 'uglify']);
+  };
+  
